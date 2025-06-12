4dbb51cdd9e19d254d13e0c88052aebb3d7ab8d6
qian <ruiqian.tang@seeed.cc>
2025-06-12 13:55:28 +0800

    fix IMU does not provide any data.

--- src/imu.c
+++ src/imu.c
@@ -1,76 +1,65 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/shell/shell.h>
+#include <zephyr/drivers/gpio.h>
+#include <nrfx.h>
+#include <zephyr/dt-bindings/gpio/nordic-nrf-gpio.h>
 
-static const struct device *const imu = DEVICE_DT_GET(DT_ALIAS(imu0));
-static bool initialized;
-
+static const struct device *const imu = DEVICE_DT_GET(DT_NODELABEL(lsm6dso));
 static int cmd_imu_get(const struct shell *sh, size_t argc, char **argv)
 {
-	int err;
-	struct sensor_value odr, acc_data[3], gyro_data[3];
+    int ret;
+    struct sensor_value accel_data[3];
+    struct sensor_value gyro_data[3];
+    struct sensor_value odr_attr;
+    /* set accel/gyro sampling frequency to 12.5 Hz */
+    odr_attr.val1 = 12.5;
+    odr_attr.val2 = 0;
+    // gpio_pin_set_dt(&lsm6dso_en, 1);
 
-	ARG_UNUSED(argc);
-	ARG_UNUSED(argv);
+    if (!device_is_ready(imu)) {
+        shell_error(sh, "Device not ready\n");
+        return -ENODEV;
+    }
 
-	if (!initialized) {
-		shell_error(sh, "IMU sensor module not initialized");
-		return -EPERM;
-	}
+    ret = sensor_attr_set(imu, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_SAMPLING_FREQUENCY, &odr_attr);
+    if (ret)
+    {
+        shell_error(sh, "Failed to set accel sampling frequency\n");
+        return ret;
+    }
 
-	/* ODR: 12.5 Hz */
-	odr.val1 = 12;
-	odr.val2 = 0;
+    ret = sensor_attr_set(imu, SENSOR_CHAN_GYRO_XYZ, SENSOR_ATTR_SAMPLING_FREQUENCY, &odr_attr);
+    if (ret)
+    {
+        shell_error(sh, "Failed to set gyro sampling frequency\n");
+        return ret;
+    }
 
-	err = sensor_attr_set(imu, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_SAMPLING_FREQUENCY, &odr);
-	if (err < 0) {
-		return err;
-	}
+    ret = sensor_sample_fetch(imu);
+    if (ret)
+    {
+        shell_error(sh, "Failed to fetch sample\n");
+        return ret;
+    }
 
-	err = sensor_attr_set(imu, SENSOR_CHAN_GYRO_XYZ, SENSOR_ATTR_SAMPLING_FREQUENCY, &odr);
-	if (err < 0) {
-		return err;
-	}
+    ret = sensor_channel_get(imu, SENSOR_CHAN_ACCEL_XYZ, accel_data);
+    if (ret)
+    {
+        shell_error(sh, "Failed to get accel data\n");
+        return ret;
+    }
 
-	err = sensor_sample_fetch(imu);
-	if (err < 0) {
-		shell_error(sh, "Failed to fetch sensor data (%d)", err);
-		return 0;
-	}
+    ret = sensor_channel_get(imu, SENSOR_CHAN_GYRO_XYZ, gyro_data);
+    if (ret)
+    {
+        shell_error(sh, "Failed to get gyro data\n");
+        return ret;
+    }
 
-	err = sensor_channel_get(imu, SENSOR_CHAN_ACCEL_XYZ, acc_data);
-	if (err < 0) {
-		shell_error(sh, "Failed to get accelerometer data (%d)", err);
-		return 0;
-	}
-
-	err = sensor_channel_get(imu, SENSOR_CHAN_GYRO_XYZ, gyro_data);
-	if (err < 0) {
-		shell_error(sh, "Failed to get gyroscope data (%d)", err);
-		return 0;
-	}
-
-	shell_print(sh, "Acceleration (m/s2): %.6f, %.6f, %.6f",
-		    sensor_value_to_double(&acc_data[0]), sensor_value_to_double(&acc_data[1]),
-		    sensor_value_to_double(&acc_data[2]));
-	shell_print(sh, "Angular velocity (rad/s): %.6f, %.6f, %.6f",
-		    sensor_value_to_double(&gyro_data[0]), sensor_value_to_double(&gyro_data[1]),
-		    sensor_value_to_double(&gyro_data[2]));
-
-	/* ODR: 0 (power-down) */
-	odr.val1 = 0;
-	odr.val2 = 0;
-
-	err = sensor_attr_set(imu, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_SAMPLING_FREQUENCY, &odr);
-	if (err < 0) {
-		return err;
-	}
-
-	err = sensor_attr_set(imu, SENSOR_CHAN_GYRO_XYZ, SENSOR_ATTR_SAMPLING_FREQUENCY, &odr);
-	if (err < 0) {
-		return err;
-	}
-
-	return 0;
+    // gpio_pin_set_dt(&lsm6dso_en, 0);
+    shell_print(sh, "accel data: %d.%06d, %d.%06d, %d.%06d", accel_data[0].val1, accel_data[0].val2, accel_data[1].val1, accel_data[1].val2, accel_data[2].val1, accel_data[2].val2);
+    shell_print(sh, "gyro data: %d.%06d, %d.%06d, %d.%06d", gyro_data[0].val1, gyro_data[0].val2, gyro_data[1].val1, gyro_data[1].val2, gyro_data[2].val1, gyro_data[2].val2);
+    return ret;
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_imu_cmds, 
@@ -79,13 +68,25 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_imu_cmds,
 
 SHELL_CMD_REGISTER(imu, &sub_imu_cmds, "IMU sensor", NULL);
 
-int imu_init(void)
+static const struct gpio_dt_spec lsm6dso_en = GPIO_DT_SPEC_GET_OR(DT_NODELABEL(pdm_imu_en_pin), gpios, {0});
+static int imu_poweron(void)
 {
-	if (!device_is_ready(imu)) {
-		return -ENODEV;
-	}
+    int ret;
 
-	initialized = true;
+    ret = gpio_pin_configure_dt(&lsm6dso_en, (GPIO_OUTPUT | NRF_GPIO_DRIVE_S0H1));
+    if (ret < 0)
+    {
+        return ret;
+    }
 
-	return 0;
+    ret = gpio_pin_set_dt(&lsm6dso_en, 1);
+    if (ret < 0)
+    {
+        return ret;
+    }
+    k_sleep(K_MSEC(50));
+
+    return 0;
 }
+
+SYS_INIT(imu_poweron, POST_KERNEL, 89);
--- src/imu.h
+++
@@ -1,6 +0,0 @@
-#ifndef APP_SRC_IMU_H_
-#define APP_SRC_IMU_H_
-
-int imu_init(void);
-
-#endif /* APP_SRC_IMU_H_ */

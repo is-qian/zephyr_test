cfeccd7e8ba37de7b287305c9e60f8d0d73304b7
qian <ruiqian.tang@seeed.cc>
2025-07-04 15:31:40 +0800

    Add the command for reading battery voltage

--- src/adc.c
+++ src/adc.c
@@ -22,10 +22,9 @@ LOG_MODULE_REGISTER(adc);
 /* Data of ADC io-channels specified in devicetree. */
 static const struct adc_dt_spec adc_channels[] = {
 	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
-			     DT_SPEC_AND_COMMA)
-};
+						 DT_SPEC_AND_COMMA)};
 
-static const struct device *const  vbat_reg = DEVICE_DT_GET(DT_NODELABEL(vbat_pwr));
+static const struct device *const vbat_reg = DEVICE_DT_GET(DT_NODELABEL(vbat_pwr));
 
 static int cmd_adc_get(const struct shell *sh, size_t argc, char **argv)
 {
@@ -37,76 +36,155 @@ static int cmd_adc_get(const struct shell *sh, size_t argc, char **argv)
 		.buffer_size = sizeof(buf),
 	};
 
-	regulator_disable(vbat_reg);
-
+	regulator_enable(vbat_reg);
+	k_sleep(K_MSEC(100));
+	
 	/* Configure channels individually prior to sampling. */
-	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-		if (!adc_is_ready_dt(&adc_channels[i])) {
+	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++)
+	{
+		if (!adc_is_ready_dt(&adc_channels[i]))
+		{
 			shell_error(sh, "ADC controller device %s not ready\n", adc_channels[i].dev->name);
 			return 0;
 		}
 
 		err = adc_channel_setup_dt(&adc_channels[i]);
-		if (err < 0) {
+		if (err < 0)
+		{
 			shell_error(sh, "Could not setup channel #%d (%d)\n", i, err);
 			return 0;
 		}
 	}
 
-		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-			int32_t val_mv;
+	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++)
+	{
+		int32_t val_mv;
 
-			shell_print(sh, "channel %d: ",
-			       adc_channels[i].channel_id);
+		shell_print(sh, "channel %d: ",
+					adc_channels[i].channel_id);
 
-			(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
+		(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
 
-			err = adc_read_dt(&adc_channels[i], &sequence);
-			if (err < 0) {
-				shell_error(sh, "Could not read (%d)\n", err);
-				continue;
-			}
-
-			/*
-			 * If using differential mode, the 16 bit value
-			 * in the ADC sample buffer should be a signed 2's
-			 * complement value.
-			 */
-			if (adc_channels[i].channel_cfg.differential) {
-				val_mv = (int32_t)((int16_t)buf);
-			} else {
-				val_mv = (int32_t)buf;
-			}
-			shell_print(sh, "		raw:%"PRId32, val_mv);
-			err = adc_raw_to_millivolts_dt(&adc_channels[i],
-						       &val_mv);
-			/* conversion to mV may not be supported, skip if not */
-			if (err < 0) {
-				shell_error(sh, " (value in mV not available)\n");
-			} else {
-				shell_print(sh, "		vol = %"PRId32" mV\n", val_mv);
-			}
+		err = adc_read_dt(&adc_channels[i], &sequence);
+		if (err < 0)
+		{
+			shell_error(sh, "Could not read (%d)\n", err);
+			continue;
 		}
 
+		/*
+		 * If using differential mode, the 16 bit value
+		 * in the ADC sample buffer should be a signed 2's
+		 * complement value.
+		 */
+		if (adc_channels[i].channel_cfg.differential)
+		{
+			val_mv = (int32_t)((int16_t)buf);
+		}
+		else
+		{
+			val_mv = (int32_t)buf;
+		}
+		shell_print(sh, "		raw:%" PRId32, val_mv);
+		err = adc_raw_to_millivolts_dt(&adc_channels[i],
+									   &val_mv);
+		/* conversion to mV may not be supported, skip if not */
+		if (err < 0)
+		{
+			shell_error(sh, " (value in mV not available)\n");
+		}
+		else
+		{
+			shell_print(sh, "		vol = %" PRId32 " mV\n", val_mv);
+		}
+	}
+
 	regulator_disable(vbat_reg);
-    return 0;
+	return 0;
+}
+
+static int cmd_bat_get(const struct shell *sh, size_t argc, char **argv)
+{
+	int err;
+	uint16_t buf;
+	int32_t val_mv;
+	struct adc_sequence sequence = {
+		.buffer = &buf,
+		/* buffer size in bytes, not number of samples */
+		.buffer_size = sizeof(buf),
+	};
+
+	regulator_enable(vbat_reg);
+	k_sleep(K_MSEC(100));
+
+	/* Configure channels individually prior to sampling. */
+	if (!adc_is_ready_dt(&adc_channels[7]))
+	{
+		shell_error(sh, "ADC controller device %s not ready\n", adc_channels[7].dev->name);
+		return 0;
+	}
+
+	err = adc_channel_setup_dt(&adc_channels[7]);
+	if (err < 0)
+	{
+		shell_error(sh, "Could not setup channel #7 (%d)\n", err);
+		return 0;
+	}
+
+	(void)adc_sequence_init_dt(&adc_channels[7], &sequence);
+
+	err = adc_read_dt(&adc_channels[7], &sequence);
+	if (err < 0)
+	{
+		shell_error(sh, "Could not read (%d)\n", err);
+		return 0;
+	}
+
+	/*
+	 * If using differential mode, the 16 bit value
+	 * in the ADC sample buffer should be a signed 2's
+	 * complement value.
+	 */
+	if (adc_channels[7].channel_cfg.differential)
+	{
+		val_mv = (int32_t)((int16_t)buf);
+	}
+	else
+	{
+		val_mv = (int32_t)buf;
+	}
+	err = adc_raw_to_millivolts_dt(&adc_channels[7],
+								   &val_mv);
+	/* conversion to mV may not be supported, skip if not */
+	if (err < 0)
+	{
+		shell_error(sh, " value in mV not available\n");
+	}
+	else
+	{
+		shell_print(sh, "bat vol = %" PRId32 " mV\n", val_mv * 2);
+	}
+
+	regulator_disable(vbat_reg);
+	return 0;
 }
 
 int adc_init(void)
-{ 
-    int err;
-    err = regulator_disable(vbat_reg);
-    if (err < 0)
-    {
-        LOG_ERR("Failed to configure enable pin (%d)", err);
-        return err;
-    }
+{
+	int err;
+	err = regulator_disable(vbat_reg);
+	if (err < 0)
+	{
+		LOG_ERR("Failed to configure enable pin (%d)", err);
+		return err;
+	}
 
-    return 0;
+	return 0;
 }
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_adc_cmds,
-                               SHELL_CMD(get, NULL, "Get ADC voltage", cmd_adc_get),
-                               SHELL_SUBCMD_SET_END);
+							   SHELL_CMD(get, NULL, "Get ADC voltage", cmd_adc_get),
+							   SHELL_CMD(bat, NULL, "Get bat voltage", cmd_bat_get),
+							   SHELL_SUBCMD_SET_END);
 
 SHELL_CMD_REGISTER(adc_read, &sub_adc_cmds, "Get ADC voltage", NULL);
